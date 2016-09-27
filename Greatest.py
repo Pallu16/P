0639ea495e5d9451fee5a1877586c0b5038ccdb8
Pallu16 <pallavikusmude0@gmail.com>
2016-09-27 11:20:30 +0530

    Add files via upload

---
+++ Greatest.py
@@ -0,0 +1,9 @@
+def great(a,b):
+    if(a>b):
+        return a
+    else:
+            return b
+a=int(input("enter a"))
+b=int(input("enter b"))
+result=great(a,b)
+print("greatest number is",result)
